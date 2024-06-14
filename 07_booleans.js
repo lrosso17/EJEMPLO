f2778fcd7af60a388ce41738002028d49d9c60df
Lizeth Rosso <lizeth.rosso@gmail.com>
2024-06-13 21:07:37 -0500

    agregando 07_y_08.js

---
+++ 07_booleans.js
@@ -0,0 +1,18 @@
+// **********  6. Booleans   **********
+let a = 'Demo'
+let b = 23
+let c = undefined
+
+let verdadero;
+verdadero = true
+const verdad = true;
+let falso = false;
+let v = Boolean(true);
+let f = Boolean(false);
+
+console.log(verdadero);
+console.log(verdadero, falso, v, f);
+console.log(verdad);
+
+console.log(typeof a, typeof b, typeof c);
+console.log(typeof verdadero, typeof falso);
