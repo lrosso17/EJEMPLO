957767268cc66256bfbecedb2fd5878b7e0035d2
Lizeth Rosso <lizeth.rosso@gmail.com>
2024-06-13 19:04:50 -0500

    agregando archivo_0.js

---
+++ archivo_0.js
@@ -0,0 +1,12 @@
+let alumno = ['Lizeth', 'Sergio', 'Johan']
+console.log(alumno)
+
+// * master --> crea desde el ambiente local
+// ! main --> crea desde github 
+
+// * git init --> Inicializamos el proyecto con git  
+// * git add . --> Agregar el archivo al stage (nuevo, modificacion o eliminacion) 
+// * git commit -m --> Agregando un comentario para identificar el cambio
+// ^ git status --> identificamos cambios en el stage 
+// * git push --> enviamos los cambios a github 
+// * git pull origin master --> Traemos
