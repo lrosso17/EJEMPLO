a98da087c9b5921028be29a0bb53c3a41824e131
Lizeth Rosso <lizeth.rosso@gmail.com>
2024-06-13 20:33:56 -0500

    agregando 05_template_strings.js

---
+++ 05_template_strings.js
@@ -0,0 +1,29 @@
+// //*********4. Template strings- **********/
+
+// let nombre ='Sergio'
+// let cargo = 'QA AUTOMATION'
+// let empresa = 'OKA'
+
+// //let persona = nombre + cargo + empresa no se debe hacer asi
+
+// let persona = "Hola soy:" + nombre + " y soy" + cargo + " y trabajo en " + empresa;
+// console.log(persona)
+// //alt + 96 para sacar  o alt gr y llave `
+// let empleado = `Hola soy ${nombre} y tengo el cargo de ${cargo} y actualmente trabajo en ${empresa}`;
+// console.log(empleado)
+
+
+// **********  4. Template Strings -  **********
+
+let nombre = 'Sergio'
+let cargo = 'QA AUTOMATION'
+let empresa = 'OKA'
+
+let persona0 = nombre + cargo + empresa
+console.log(persona0)
+
+let persona = "Hola soy: " + nombre + " y soy " + cargo + " y trabajo en " + empresa;
+console.log(persona)
+
+let empleado = `Hola soy ${nombre} y tengo el cargo de ${cargo} y actualmente trabajo en ${empresa}`
+console.log(empleado)
